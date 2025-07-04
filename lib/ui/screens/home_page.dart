3779379d87a6182beebbecd06570ad9d1e77f100
Felipe Ramos de Freitas <felipe.freitas@populisrh.com>
2025-07-04 13:15:38 -0300

    feat: criando tela de filmes e reutilizando botão

--- lib/ui/screens/home_page.dart
+++ lib/ui/screens/home_page.dart
@@ -1,4 +1,5 @@
-import 'package:cinetopia/ui/components/bottons.dart';
+import 'package:cinetopia/ui/components/buttons.dart';
+import 'package:cinetopia/ui/screens/search_movies.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -36,7 +37,16 @@ class HomePage extends StatelessWidget {
                     textAlign: TextAlign.center,
                   ),
                 ),
-                PrimaryButton(),
+                PrimaryButton(
+                  text: 'Quero Começar!',
+                  icon: Icons.arrow_forward,
+                  onTap: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => SearchMovies()
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
