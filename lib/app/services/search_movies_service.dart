d52c4ba68eae8ad80a9477ce2de1a5782d098c25
Felipe Ramos de Freitas <felipe.freitas@populisrh.com>
2025-07-05 11:14:52 -0300

    feat: usando futureBuilder

--- lib/app/services/search_movies_service.dart
+++ lib/app/services/search_movies_service.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-
 import 'package:cinetopia/app/models/movie.dart';
 import 'package:cinetopia/app/services/api_key.dart';
 import 'package:http/http.dart' as http;
@@ -7,29 +5,28 @@ import 'package:http/http.dart' as http;
 class SearchMoviesService {
   final String popularMoviesUrl =
       "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1";
+  final List<Movie> movies = <Movie>[];
 
   Future<List<Movie>> searchMovies() async {
     try {
-      http.Response response = await http.get(
+      final response = await http.get(
         Uri.parse(popularMoviesUrl),
         headers: {
           'accept': 'application/json',
           'Authorization': 'Bearer $apiToken',
         },
       );
-      Map<String, dynamic> mapResponse = json.decode(response.body);
-      List<dynamic> listDynamic = json.decode(mapResponse["results"]);
-      List<Movie> listMovies = [];
 
-      for (dynamic dyn in listDynamic) {
-        Map<String, dynamic> mapMovie = dyn as Map<String, dynamic>;
-        Movie movie = Movie.fromMap(mapMovie);
-        listMovies.add(movie);
+      if (response.statusCode == 200) {
+        print(response.body);
+      } else {
+        throw Exception(response.body);
       }
 
-      return listMovies;
+      return movies;
     } catch (e) {
-      return [];
+      print(e);
+      return movies;
     }
   }
 }
--- lib/ui/screens/search_movies.dart
+++ lib/ui/screens/search_movies.dart
@@ -1,46 +1,67 @@
+import 'package:cinetopia/app/services/search_movies_service.dart';
 import 'package:cinetopia/ui/components/movie_card.dart';
 import 'package:flutter/material.dart';
 
 class SearchMovies extends StatelessWidget {
-  const SearchMovies({super.key});
+  final SearchMoviesService searchMoviesService = SearchMoviesService();
+  SearchMovies({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(color: Color(0xFF1D0E44)),
-      child: CustomScrollView(
-        slivers: <Widget>[
-          SliverToBoxAdapter(
-            child: Image.asset("assets/popular.png", height: 80, width: 80),
-          ),
-          SliverToBoxAdapter(
-            child: Center(
-              child: Text(
-                "Filmes Populares",
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
-              ),
-            ),
-          ),
-          SliverToBoxAdapter(
-            child: Padding(
-              padding: const EdgeInsets.all(16),
-              child: TextField(
-                decoration: InputDecoration(
-                  label: Text("Pesquisar", style: TextStyle(fontSize: 15)),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white),
-                    borderRadius: BorderRadius.all(Radius.circular(15)),
+    return FutureBuilder(
+      future: searchMoviesService.searchMovies(),
+      builder: (context, asyncSnapshot) {
+        if (asyncSnapshot.hasData) {
+          return Container(
+            decoration: BoxDecoration(color: Color(0xFF1D0E44)),
+            child: CustomScrollView(
+              slivers: <Widget>[
+                SliverToBoxAdapter(
+                  child: Image.asset(
+                    "assets/popular.png",
+                    height: 80,
+                    width: 80,
                   ),
                 ),
-              ),
+                SliverToBoxAdapter(
+                  child: Center(
+                    child: Text(
+                      "Filmes Populares",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ),
+                ),
+                SliverToBoxAdapter(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        label: Text(
+                          "Pesquisar",
+                          style: TextStyle(fontSize: 15),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                          borderRadius: BorderRadius.all(Radius.circular(15)),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SliverList.builder(
+                  itemBuilder: (context, index) => MovieCard(),
+                  itemCount: 10,
+                ),
+              ],
             ),
-          ),
-          SliverList.builder(
-            itemBuilder: (context, index) => MovieCard(),
-            itemCount: 10,
-          ),
-        ],
-      ),
+          );
+        } else {
+          return Center(child: CircularProgressIndicator());
+        }
+      },
     );
   }
 }
